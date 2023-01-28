14cea327eb16f710110ae82feb4f18218d503da1
Syed Farhan Azam Naqvi <39867162+farhanazam41@users.noreply.github.com>
2023-01-27 20:41:42 -0800

    Initial commit

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-container">
+        <li className="navbar-item">Home</li>
+        <li className="navbar-item">About</li>
+        <li className="navbar-item">Services</li>
+        <li className="navbar-item">Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
